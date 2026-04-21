60915475ec6c7ec6dce2133699f9568222c9e7ad
Cristian Murillo <crisanmur@gmail.com>
2026-04-21 20:02:12 +0000

    Initial commit

---
+++ src/jobs/PolygonAreaJob.ts
@@ -0,0 +1,22 @@
+import { Feature, Geometry, GeometryObject, Polygon } from "geojson";
+import { area } from '@turf/area';
+import {feature} from '@turf/helpers';
+import { Task } from "../models/Task";
+import { Job } from "./Job";
+
+
+export class PolygonAreaJob implements Job {
+    async run(task: Task): Promise<string> {
+        console.log(`Calculating area requested in ${task.taskId} task...`);
+
+        
+        const turfFeature: Feature<GeometryObject> = feature(JSON.parse(task.geoJson));
+
+        if (turfFeature.geometry.type === 'Polygon' || turfFeature.geometry.type === 'MultiPolygon') {
+            const areaSqm = area(turfFeature).toFixed(3);
+            console.log(`The area of the given geometry is ${areaSqm} squared meters`);
+            return areaSqm;
+        }
+        throw new Error('Invalid geometry provided. An area can only be calculated for polygons or multipolygon geometries');
+    }
+}
