bf89acf58b6000aebe849d202118b84b62cdae2a
Cristian Murillo <crisanmur@gmail.com>
2026-04-23 19:25:38 +0000

    handle error messages and task outputs in separate columns

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,39 +1,43 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToOne, OneToMany, JoinTable, ManyToMany } from 'typeorm';
 import { Workflow } from './Workflow';
-import {TaskStatus} from "../workers/taskRunner";
+import { TaskStatus } from '../workers/taskRunner';
 
 @Entity({ name: 'tasks' })
 export class Task {
-    @PrimaryGeneratedColumn('uuid')
-    taskId!: string;
+	@PrimaryGeneratedColumn('uuid')
+	taskId!: string;
 
-    @Column()
-    clientId!: string;
+	@Column()
+	clientId!: string;
 
-    @Column('text')
-    geoJson!: string;
+	@Column('text')
+	geoJson!: string;
 
-    @Column()
-    status!: TaskStatus;
+	@Column()
+	status!: TaskStatus;
 
-    @Column({ nullable: true, type: 'text' })
-    progress?: string | null;
+	@Column({ nullable: true, type: 'text' })
+	progress?: string | null;
 
-    @Column({ nullable: true })
-    resultId?: string;
+	@Column({ nullable: true })
+	resultId?: string;
 
-    @Column()
-    taskType!: string;
+	@Column()
+	taskType!: string;
 
-    @Column({ default: 1 })
-    stepNumber!: number;
+	@Column({ default: 1 })
+	stepNumber!: number;
 
-    @Column({nullable: true, type: 'text'})
-    output?: string | null;
+	@Column({ nullable: true, type: 'text' })
+	output?: string | null;
 
-    @ManyToOne(() => Workflow, workflow => workflow.tasks)
-    workflow!: Workflow;
+	@ManyToOne(() => Workflow, (workflow) => workflow.tasks)
+	workflow!: Workflow;
 
-    @Column({nullable: true})
-    dependency?: number;
+	@Column({ nullable: true })
+	dependency?: number;
+
+	@Column({ nullable: true, type: 'text' })
+	errorMsg?: string | null;
 }
+
--- src/models/Workflow.ts
+++ src/models/Workflow.ts
@@ -1,21 +1,25 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm'
-import { Task } from './Task'
-import { WorkflowStatus } from '../workflows/WorkflowFactory'
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Task } from './Task';
+import { WorkflowStatus } from '../workflows/WorkflowFactory';
 
 @Entity({ name: 'workflows' })
 export class Workflow {
-    @PrimaryGeneratedColumn('uuid')
-    workflowId!: string
+	@PrimaryGeneratedColumn('uuid')
+	workflowId!: string;
 
-    @Column()
-    clientId!: string
+	@Column()
+	clientId!: string;
 
-    @Column({ default: WorkflowStatus.Initial })
-    status!: WorkflowStatus
+	@Column({ default: WorkflowStatus.Initial })
+	status!: WorkflowStatus;
 
-    @Column('text')
-    finalResult?: string | null
+	@Column('text')
+	finalResult?: string | null;
 
-    @OneToMany(() => Task, (task) => task.workflow)
-    tasks!: Task[]
+	@Column('text')
+	errorMsg?: string | null;
+
+	@OneToMany(() => Task, (task) => task.workflow)
+	tasks!: Task[];
 }
+
