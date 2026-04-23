53aab34bd2157c5132d98f8a0912da68686ba698
Cristian Murillo <crisanmur@gmail.com>
2026-04-23 20:07:57 +0000

    created endpoint for querying workflow results

--- src/routes/workflowRoutes.ts
+++ src/routes/workflowRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AppDataSource } from '../data-source';
 import { Workflow } from '../models/Workflow';
 import { TaskStatus } from '../workers/taskRunner';
+import { WorkflowStatus } from '../workflows/WorkflowFactory';
 
 const workflowRouter = Router();
 
@@ -29,6 +30,31 @@ workflowRouter.get('/:id/status', async (req, res) => {
 		}
 	} catch {
 		res.status(500).json({ message: `An error ocurred while querying status for workflow ${workflowId}` });
+	}
+});
+
+workflowRouter.get('/:id/results', async (req, res) => {
+	const workflowId = req.params.id;
+	try {
+		const workflowRepository = AppDataSource.manager.getRepository(Workflow);
+		const workflow = await workflowRepository.findOne({
+			where: {
+				workflowId,
+			},
+			relations: ['tasks'],
+		});
+
+		if (!workflow) {
+			res.status(404).json({ message: `Workflow ${workflowId} not found` });
+		} else if (![WorkflowStatus.Completed, WorkflowStatus.Failed].includes(workflow.status)) {
+			res.status(400).json({
+				message: `Worklow ${workflowId} ${workflow.status === WorkflowStatus.InProgress ? 'is still runnig' : 'has not been started yet.'}`,
+			});
+		} else {
+			res.status(200).json(JSON.parse(workflow.finalResult ?? '{}'));
+		}
+	} catch {
+		res.status(500).json({ message: `An error ocurred while trying to retrieve results for workflow ${workflowId}` });
 	}
 });
 
